77a90de19dc0b454ad32d3148daa9e0fd737633a
Suhas Cristy Mathey <132161396+Cristy210@users.noreply.github.com>
2024-09-19 20:25:51 -0400

    Julia Notebook for Loading the Data

---
+++ Data_Loading.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.19.46
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 24f463ee-76e6-11ef-0b34-796df7c80881
+import Pkg; Pkg.activate(@__DIR__)
+
+# ╔═╡ 0fd3fb14-34f4-419b-bfe4-9eff238cc439
+using CairoMakie, LinearAlgebra, Colors, PlutoUI
+
+# ╔═╡ ab3e1205-ed61-408f-968f-9b91a572005e
+
+
+# ╔═╡ Cell order:
+# ╠═24f463ee-76e6-11ef-0b34-796df7c80881
+# ╠═0fd3fb14-34f4-419b-bfe4-9eff238cc439
+# ╠═ab3e1205-ed61-408f-968f-9b91a572005e
