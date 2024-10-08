87ce3e2841e51da4ba3f3600ddfa0f8800e0a0bb
Suhas Cristy Mathey <132161396+Cristy210@users.noreply.github.com>
2024-10-08 16:23:25 -0400

    Data Loading for a new HSI dataset

--- Data_Loading.jl
+++ Data_Loading.jl
@@ -18,7 +18,7 @@ end
 import Pkg; Pkg.activate(@__DIR__)
 
 # ╔═╡ 0fd3fb14-34f4-419b-bfe4-9eff238cc439
-using CairoMakie, LinearAlgebra, Colors, PlutoUI, ArchGDAL, Glob, FileIO
+using CairoMakie, LinearAlgebra, Colors, PlutoUI, ArchGDAL, Glob, FileIO, ArnoldiMethod, CacheVariables
 
 # ╔═╡ 8376c0c9-7030-4425-83ad-be7c99609b7d
 md"""
@@ -39,86 +39,69 @@ md"""
 ### Loading Julia Packages
 """
 
-# ╔═╡ 46bc740c-8109-4cb0-931b-099b29154426
-@bind City Select(["abudhabi", "mumbai"])
+# ╔═╡ 18e316a8-9ec6-4343-8cf4-7a04eccd0df8
+@bind item Select(["coffee", "rice", "sugar_salt_flour", "sugar_salt_flour_contamination", "yatsuhashi"])
 
 # ╔═╡ 40be7a21-175b-4032-af72-3b2258117402
 md"""
-### Storing the directory path for MSI files in Img1 and Img2 variables
+### Storing the directory path for HSI files
 """
 
-# ╔═╡ ab3e1205-ed61-408f-968f-9b91a572005e
-Img1_Path = joinpath(@__DIR__, 
-	"Onera Satellite Change Detection dataset - Images",
-	City,
-	"imgs_1_rect")
+# ╔═╡ 51d5b05d-683c-4d96-bc16-a6ef972f0d04
+data_path = joinpath(@__DIR__, "NIR_HSI_Coffee_DS", "data", item)
 
-# ╔═╡ 692b666b-b45b-4063-a7e4-4f5d494443fe
-Img2_Path = joinpath(@__DIR__, 
-	"Onera Satellite Change Detection dataset - Images",
-	City,
-	"imgs_2_rect")
+# ╔═╡ 74af4aed-0e24-4045-90ca-aa14a0284a44
+files = glob("*.png", data_path)
 
-# ╔═╡ 41d6de1e-0fdb-4658-a0ee-aa87b0366c6e
-md"""
-### Storing all 13 *.tif files in I1 and I2 variables
-"""
+# ╔═╡ 4dab33d9-3b14-4615-88a5-11fa0239ba65
+Array = [Float64.(load(files[i])) for i in 1:length(files)]
 
-# ╔═╡ 67de718c-65f5-4b87-b31e-6915316778e5
-I1 = glob("*.tif", Img1_Path)
+# ╔═╡ b67c80d7-be13-4e79-a14b-9aa6fea7eb78
+data_tens = cat(Array..., dims=3)
 
-# ╔═╡ 259b1fc5-a3ac-4bbb-bb93-73b48eebd780
-I2 = glob("*.tif", Img2_Path)
+# ╔═╡ a6b8a27b-614c-42ba-941e-792f586b2881
+trimmed = data_tens[20:end, 30:end, :]
 
-# ╔═╡ 59260065-50e7-4f4d-9460-ae77c63ac34b
-GT1_Path = joinpath(@__DIR__, 
-	"Onera Satellite Change Detection dataset - Images",
-	City,
-	"pair",
-	"img1.png")
+# ╔═╡ 18262d61-d4d2-457e-a79a-77c3a365042c
+@bind band PlutoUI.Slider(1:size(data_tens, 3), show_value=true)
 
-# ╔═╡ 9fa99b74-510e-415b-9db8-5954a359fc02
-GT2_Path = joinpath(@__DIR__, 
-	"Onera Satellite Change Detection dataset - Images",
-	City,
-	"pair",
-	"img2.png")
-
-# ╔═╡ 06d2f65b-73bc-4114-8c21-70d1e36e4aaf
-md"""
-#### Al 13 matrices are stored in the Img1 and Img2 variables
-"""
-
-# ╔═╡ f893213d-ef21-4b33-84de-8e3a51cd71f4
-Img1 = [ArchGDAL.read(tif_file) do dataset band = ArchGDAL.getband(dataset, 1); data = ArchGDAL.read(band) end for tif_file in I1]
-
-# ╔═╡ 4e390cf0-016e-4b83-b3ef-6c66ee325c69
-Img2 = [ArchGDAL.read(tif_file) do dataset band = ArchGDAL.getband(dataset, 1); data = ArchGDAL.read(band) end for tif_file in I2]
-
-# ╔═╡ 551bccd4-54e0-4f7e-af41-a6063889e464
-tens1 = cat(Img1..., dims=3);
-
-# ╔═╡ a2d1e360-b7b0-4301-9375-11fba3ecf911
-@bind Band PlutoUI.Slider(1:13, show_value=true)
-
-# ╔═╡ cde91b75-74ff-45d9-b3c0-46873e8a8be7
+# ╔═╡ 88fa84cd-b785-4bf7-841e-3c623c381c86
 with_theme() do
-	fig = Figure(; size=(800, 600))
-	ax = Axis(fig[1, 1], aspect = DataAspect(), yreversed=true)
-	image!(ax, tens1[:, :, Band])
+	fig = Figure(; size=(600, 800))
+	ax = Axis(fig[1, 1], aspect=DataAspect(), yreversed=true)
+	image!(ax, permutedims(data_tens[:, :, band]))
 	fig
 end
 
-# ╔═╡ 82770fa3-323b-4d7c-b8e8-2aad72c89858
-tens2 = cat(Img2..., dims=3);
+# ╔═╡ 2b027eda-4ec5-4315-bc7e-5a7774abac9b
+data_mat = permutedims(reshape(data_tens, :, size(data_tens, 3)))
 
-# ╔═╡ bb5b4703-eb92-47ae-9d23-c1db7c096e6f
-with_theme() do
-	fig = Figure(; size=(800, 600))
-	ax = Axis(fig[1, 1], aspect = DataAspect(), yreversed=true)
-	image!(ax, tens2[:, :, Band])
-	fig
-end
+# ╔═╡ 6794a588-7c72-47dd-8395-164545b6132a
+col_norms = [norm(data_mat[:, i]) for i in 1:size(data_mat, 2)]
+
+# ╔═╡ 3835fef4-ab2a-44bf-a3b5-0bce1a70c605
+Norm_vec = [data_mat[:, i] ./ col_norms[i] for i in 1:size(data_mat, 2)]
+
+# ╔═╡ 0050899c-2616-4f7f-aac7-c0a2eefb6ca2
+Norm_mat = hcat(Norm_vec...)
+
+# ╔═╡ 36c193e0-7531-4120-91e0-3e0ecdcb65bc
+# A = transpose(Norm_mat) * Norm_mat
+
+# ╔═╡ 0d12ebec-75c3-4f15-8098-9867bb6a3626
+# S = exp.((-2 .* acos.(clamp.(A, -1, 1))))
+
+# ╔═╡ b81de22d-469a-41e4-acf4-538d395bc653
+# diag_mat = Diagonal(vec(sum(S, dims=2)))
+
+# ╔═╡ 4f58a7ac-ae6e-4e43-9d59-8a5d1eab6554
+# D_sqrinv = sqrt(inv(diag_mat))
+
+# ╔═╡ 9982c4a7-bd44-4f04-a241-2eeeefaaa0fb
+# L_sym = Symmetric(I - (D_sqrinv * S * D_sqrinv))
+
+# ╔═╡ a59ce994-3c51-4613-ac10-7fda447ef3af
+
 
 # ╔═╡ Cell order:
 # ╠═8376c0c9-7030-4425-83ad-be7c99609b7d
@@ -126,20 +109,22 @@ end
 # ╠═24f463ee-76e6-11ef-0b34-796df7c80881
 # ╟─62790b6a-c264-44f0-aeca-8116bd9a8471
 # ╠═0fd3fb14-34f4-419b-bfe4-9eff238cc439
-# ╠═46bc740c-8109-4cb0-931b-099b29154426
+# ╠═18e316a8-9ec6-4343-8cf4-7a04eccd0df8
 # ╟─40be7a21-175b-4032-af72-3b2258117402
-# ╠═ab3e1205-ed61-408f-968f-9b91a572005e
-# ╠═692b666b-b45b-4063-a7e4-4f5d494443fe
-# ╟─41d6de1e-0fdb-4658-a0ee-aa87b0366c6e
-# ╠═67de718c-65f5-4b87-b31e-6915316778e5
-# ╠═259b1fc5-a3ac-4bbb-bb93-73b48eebd780
-# ╠═59260065-50e7-4f4d-9460-ae77c63ac34b
-# ╠═9fa99b74-510e-415b-9db8-5954a359fc02
-# ╠═06d2f65b-73bc-4114-8c21-70d1e36e4aaf
-# ╠═f893213d-ef21-4b33-84de-8e3a51cd71f4
-# ╠═4e390cf0-016e-4b83-b3ef-6c66ee325c69
-# ╠═551bccd4-54e0-4f7e-af41-a6063889e464
-# ╠═a2d1e360-b7b0-4301-9375-11fba3ecf911
-# ╠═cde91b75-74ff-45d9-b3c0-46873e8a8be7
-# ╠═82770fa3-323b-4d7c-b8e8-2aad72c89858
-# ╠═bb5b4703-eb92-47ae-9d23-c1db7c096e6f
+# ╠═51d5b05d-683c-4d96-bc16-a6ef972f0d04
+# ╠═74af4aed-0e24-4045-90ca-aa14a0284a44
+# ╠═4dab33d9-3b14-4615-88a5-11fa0239ba65
+# ╠═b67c80d7-be13-4e79-a14b-9aa6fea7eb78
+# ╠═a6b8a27b-614c-42ba-941e-792f586b2881
+# ╠═18262d61-d4d2-457e-a79a-77c3a365042c
+# ╠═88fa84cd-b785-4bf7-841e-3c623c381c86
+# ╠═2b027eda-4ec5-4315-bc7e-5a7774abac9b
+# ╠═6794a588-7c72-47dd-8395-164545b6132a
+# ╠═3835fef4-ab2a-44bf-a3b5-0bce1a70c605
+# ╠═0050899c-2616-4f7f-aac7-c0a2eefb6ca2
+# ╠═36c193e0-7531-4120-91e0-3e0ecdcb65bc
+# ╠═0d12ebec-75c3-4f15-8098-9867bb6a3626
+# ╠═b81de22d-469a-41e4-acf4-538d395bc653
+# ╠═4f58a7ac-ae6e-4e43-9d59-8a5d1eab6554
+# ╠═9982c4a7-bd44-4f04-a241-2eeeefaaa0fb
+# ╠═a59ce994-3c51-4613-ac10-7fda447ef3af
