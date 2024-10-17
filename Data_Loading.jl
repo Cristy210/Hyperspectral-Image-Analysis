052fc15dc30c7754ebe8eca9328b46d40d33ae38
Suhas Cristy Mathey <132161396+Cristy210@users.noreply.github.com>
2024-10-17 16:17:34 -0400

    Spectral Clustering implementation on NIR HS Coffee Dataset

--- Data_Loading.jl
+++ Data_Loading.jl
@@ -18,7 +18,7 @@ end
 import Pkg; Pkg.activate(@__DIR__)
 
 # ╔═╡ 0fd3fb14-34f4-419b-bfe4-9eff238cc439
-using CairoMakie, LinearAlgebra, Colors, PlutoUI, ArchGDAL, Glob, FileIO, ArnoldiMethod, CacheVariables
+using CairoMakie, LinearAlgebra, Colors, PlutoUI, Glob, FileIO, ArnoldiMethod, CacheVariables, Clustering, ProgressLogging, Dates, SparseArrays, Random, Logging
 
 # ╔═╡ 8376c0c9-7030-4425-83ad-be7c99609b7d
 md"""
@@ -42,6 +42,18 @@ md"""
 # ╔═╡ 18e316a8-9ec6-4343-8cf4-7a04eccd0df8
 @bind item Select(["coffee", "rice", "sugar_salt_flour", "sugar_salt_flour_contamination", "yatsuhashi"])
 
+# ╔═╡ 4d453d4e-c7b0-4af0-9df4-16957539c621
+n_cluster_map = Dict(
+	"coffee" => 7,
+	"rice" => 4,
+	"sugar_salt_flour" => 3,
+	"sugar_salt_flour_contamination" => 4,
+	"yatsuhashi" => 3
+)
+
+# ╔═╡ 2f419b6e-ae3f-42b3-b854-8a290e3239cd
+n_clusters = n_cluster_map[item]
+
 # ╔═╡ 40be7a21-175b-4032-af72-3b2258117402
 md"""
 ### Storing the directory path for HSI files
@@ -56,75 +68,235 @@ files = glob("*.png", data_path)
 # ╔═╡ 4dab33d9-3b14-4615-88a5-11fa0239ba65
 Array = [Float64.(load(files[i])) for i in 1:length(files)]
 
-# ╔═╡ b67c80d7-be13-4e79-a14b-9aa6fea7eb78
-data_tens = cat(Array..., dims=3)
+# ╔═╡ 3018818b-a409-46d4-9cd1-92f6113006dc
+CACHEDIR = joinpath(@__DIR__, "cache_files")
 
-# ╔═╡ a6b8a27b-614c-42ba-941e-792f586b2881
-trimmed = data_tens[20:end, 30:end, :]
+# ╔═╡ b67c80d7-be13-4e79-a14b-9aa6fea7eb78
+data = cat(Array..., dims=3)
+
+# ╔═╡ 62d7769b-dcdc-466b-84a7-df500ffe4b16
+function cachet(@nospecialize(f), path)
+	whenrun, timed_results = cache(path) do
+		return now(), @timed f()
+	end
+	@info "Was run at $whenrun (runtime = $(timed_results.time) seconds)"
+	timed_results.value
+end
+
+# ╔═╡ b4d1ea02-c7ad-4269-acc2-d34ea75acd26
+
 
 # ╔═╡ 18262d61-d4d2-457e-a79a-77c3a365042c
-@bind band PlutoUI.Slider(1:size(data_tens, 3), show_value=true)
+@bind band PlutoUI.Slider(1:size(data, 3), show_value=true)
 
 # ╔═╡ 88fa84cd-b785-4bf7-841e-3c623c381c86
 with_theme() do
 	fig = Figure(; size=(600, 800))
 	ax = Axis(fig[1, 1], aspect=DataAspect(), yreversed=true)
-	image!(ax, permutedims(data_tens[:, :, band]))
+	image!(ax, permutedims(data[:, :, band]))
 	fig
 end
 
-# ╔═╡ 2b027eda-4ec5-4315-bc7e-5a7774abac9b
-data_mat = permutedims(reshape(data_tens, :, size(data_tens, 3)))
+# ╔═╡ 5fde82bd-73bd-4528-8116-853719d74fd0
+md"""
+#### Compute Affinity Matrix
+"""
 
-# ╔═╡ 6794a588-7c72-47dd-8395-164545b6132a
-col_norms = [norm(data_mat[:, i]) for i in 1:size(data_mat, 2)]
+# ╔═╡ 953a59ea-4be0-4dd9-a49e-68a54b3d6c1a
+function affinity(cube; max_nz=10, chunksize=minimum(size(cube)[1:2]),
+	func = c -> exp(-2*acos(clamp(c,-1,1))))
 
-# ╔═╡ 3835fef4-ab2a-44bf-a3b5-0bce1a70c605
-Norm_vec = [data_mat[:, i] ./ col_norms[i] for i in 1:size(data_mat, 2)]
+	# Verify that chunksize divides the total number of pixels
+	mod(prod(size(cube)[1:2]), chunksize) == 0 ||
+		error("chunksize must divide the total number of pixels")
 
-# ╔═╡ 0050899c-2616-4f7f-aac7-c0a2eefb6ca2
-Norm_mat = hcat(Norm_vec...)
+	# Compute normalized spectra (so that inner product = cosine of angle)
+	X = permutedims(reshape(cube, :, size(cube,3)))
+	X = mapslices(normalize, X; dims=1)
 
-# ╔═╡ 36c193e0-7531-4120-91e0-3e0ecdcb65bc
-# A = transpose(Norm_mat) * Norm_mat
+	# Find nonzero values (in chunks)
+	C_buf = similar(X, size(X,2), chunksize)    # pairwise cosine buffer
+	s_buf = Vector{Int}(undef, size(X,2))       # sorting buffer
+	nz_list = @withprogress mapreduce(vcat, enumerate(Iterators.partition(1:size(X,2), chunksize))) do (chunk_idx, chunk)
+		# Compute cosine angles (for chunk) and store in buffer
+		mul!(C_buf, X', view(X, :, chunk))
 
-# ╔═╡ 0d12ebec-75c3-4f15-8098-9867bb6a3626
-# S = exp.((-2 .* acos.(clamp.(A, -1, 1))))
+		# Zero out all but `max_nz` largest values
+		nzs = map(chunk, eachcol(C_buf)) do col, c
+			idx = partialsortperm!(s_buf, c, 1:max_nz; rev=true)
+			collect(idx), fill(col, max_nz), func.(view(c,idx))
+		end
 
-# ╔═╡ b81de22d-469a-41e4-acf4-538d395bc653
-# diag_mat = Diagonal(vec(sum(S, dims=2)))
+		# Log progress and return
+		@logprogress chunk_idx/(size(X,2) ÷ chunksize)
+		return nzs
+	end
 
-# ╔═╡ 4f58a7ac-ae6e-4e43-9d59-8a5d1eab6554
-# D_sqrinv = sqrt(inv(diag_mat))
+	# Form and return sparse array
+	rows = reduce(vcat, getindex.(nz_list, 1))
+	cols = reduce(vcat, getindex.(nz_list, 2))
+	vals = reduce(vcat, getindex.(nz_list, 3))
+	return sparse([rows; cols],[cols; rows],[vals; vals])
+end
 
-# ╔═╡ 9982c4a7-bd44-4f04-a241-2eeeefaaa0fb
-# L_sym = Symmetric(I - (D_sqrinv * S * D_sqrinv))
+# ╔═╡ db91d85b-d880-44ba-b794-f957f56fc18b
+max_nz = 1800
 
-# ╔═╡ a59ce994-3c51-4613-ac10-7fda447ef3af
+# ╔═╡ ce4e3b7a-e3d7-47bb-9ca1-c68282c05ed8
+A = cachet(joinpath(CACHEDIR, "Affinity_$item$max_nz.bson")) do
+	affinity(data; max_nz)
+end
+
+# ╔═╡ 0d47164f-b858-4f3a-9fa7-d89b64ceef38
+
+
+# ╔═╡ b8442feb-8058-4fd2-bf7d-e24d81e3766f
+function embedding(A, k; seed=0)
+	# Set seed for reproducibility
+	Random.seed!(seed)
+
+	# Compute node degrees and form Laplacian
+	d = vec(sum(A; dims=2))
+	Dsqrinv = sqrt(inv(Diagonal(d)))
+	L = Symmetric(I - (Dsqrinv * A) * Dsqrinv)
+
+	# Compute eigenvectors
+	decomp, history = partialschur(L; nev=k, which=:SR)
+	@info history
+
+	return mapslices(normalize, decomp.Q; dims=2)
+end
+
+# ╔═╡ 6dd0fc08-8aeb-44f2-8d86-7d8e1d1127e1
+V = embedding(A, n_clusters)
+
+# ╔═╡ 79cfc8c3-96c1-4de9-9aab-b49e097c2019
+function batchkmeans(X, k, args...; nruns=100, kwargs...)
+	runs = @withprogress map(1:nruns) do idx
+		# Run K-means
+		Random.seed!(idx)  # set seed for reproducibility
+		result = with_logger(NullLogger()) do
+			kmeans(X, k, args...; kwargs...)
+		end
+
+		# Log progress and return result
+		@logprogress idx/nruns
+		return result
+	end
+
+	# Print how many converged
+	nconverged = count(run -> run.converged, runs)
+	@info "$nconverged/$nruns runs converged"
+
+	# Return runs sorted best to worst
+	return sort(runs; by=run->run.totalcost)
+end
+
+# ╔═╡ 4c90a7ae-8816-4cd0-96fe-5ce321e6d149
+spec_clusterings = batchkmeans(permutedims(V), n_clusters; maxiter=1000)
+
+# ╔═╡ 1d625348-8118-4575-b9b2-0a9408f4eed7
+spec_clusterings[2]
+
+# ╔═╡ 7a44f4fe-ea5a-45bf-b803-3caf2e1dead7
+min_index = argmin(spec_clusterings[i].totalcost for i in 1:100)
+
+# ╔═╡ 6c8e89db-fdde-406c-bbbb-d7f601fef58b
+clusters = spec_clusterings[min_index].assignments
+
+# ╔═╡ 02f41616-47c7-4ec9-9200-784627b28f67
+mat = reshape(clusters, size(data, 1), size(data, 2))
+
+# ╔═╡ 7161e4a5-2de4-408a-b176-d1c7d49075de
+with_theme() do
+	fig = Figure(; size=(600, 700))
+	ax = Axis(fig[1, 1], aspect=DataAspect(), yreversed=true)
+	colors = Makie.Colors.distinguishable_colors(n_clusters)
+	hm = heatmap!(ax, permutedims(mat); colormap=Makie.Categorical(colors))
+	Colorbar(fig[1, 2], hm)
+	fig
+end
+
+# ╔═╡ 35456f38-1c04-4d81-9bfb-e87b3cf792a0
+aligned_assignments(clusterings, baseperm=1:maximum(first(clusterings).assignments)) = map(clusterings) do clustering
+	# New labels determined by simple heuristic that aims to align different clusterings
+	thresh! = a -> (a[a .< 0.2*sum(a)] .= 0; a)
+	alignments = [thresh!(a) for a in eachrow(counts(clusterings[1], clustering))]
+	new_labels = sortperm(alignments[baseperm]; rev=true)
+
+	# Return assignments with new labels
+	return [new_labels[l] for l in clustering.assignments]
+end
+
+# ╔═╡ f4f9359d-411d-49d9-9813-6d27717ecbba
+# spec_aligned = aligned_assignments(spec_clusterings,[2,8,5,7,10,9,1,4,3,11,6,12]);
+spec_aligned = aligned_assignments(spec_clusterings,[8,5,7,10,9,1,4,3,11,6,12,2]);
+
+# ╔═╡ 0384a698-d1e6-400b-b86a-f207eb3d09a3
+
+
+# ╔═╡ cecc50d1-69ff-4ac0-8878-e606997d0d9d
+md"""
+### Rough Work
+"""
+
+# ╔═╡ f7223612-dd00-4c49-b289-97e7f6acd2d7
+# ╠═╡ disabled = true
+#=╠═╡
+mat = permutedims(reshape(data, :, size(data,3)))
+  ╠═╡ =#
+
+# ╔═╡ 5f6c6ca9-3b53-429a-b381-732f3a45f7d4
+X = mapslices(normalize, mat; dims=1)
+
+# ╔═╡ 570c4b96-7aa4-4f79-8217-cf7b9ed127d7
+C_buf = similar(X, size(X,2), minimum(size(data)[1:2]))
+
+# ╔═╡ b36bf2e8-5e68-4cdf-bf27-7fb65467be18
+s_buf = Vector{Int}(undef, size(X,2))
+
+# ╔═╡ 4e10811f-9fa3-4b9d-8a88-f2b86f33fac3
 
 
 # ╔═╡ Cell order:
 # ╠═8376c0c9-7030-4425-83ad-be7c99609b7d
-# ╟─fa6f6013-1ce6-4c0d-9d8e-c60a81987c41
+# ╠═fa6f6013-1ce6-4c0d-9d8e-c60a81987c41
 # ╠═24f463ee-76e6-11ef-0b34-796df7c80881
 # ╟─62790b6a-c264-44f0-aeca-8116bd9a8471
 # ╠═0fd3fb14-34f4-419b-bfe4-9eff238cc439
 # ╠═18e316a8-9ec6-4343-8cf4-7a04eccd0df8
+# ╠═4d453d4e-c7b0-4af0-9df4-16957539c621
+# ╠═2f419b6e-ae3f-42b3-b854-8a290e3239cd
 # ╟─40be7a21-175b-4032-af72-3b2258117402
 # ╠═51d5b05d-683c-4d96-bc16-a6ef972f0d04
 # ╠═74af4aed-0e24-4045-90ca-aa14a0284a44
 # ╠═4dab33d9-3b14-4615-88a5-11fa0239ba65
+# ╠═3018818b-a409-46d4-9cd1-92f6113006dc
 # ╠═b67c80d7-be13-4e79-a14b-9aa6fea7eb78
-# ╠═a6b8a27b-614c-42ba-941e-792f586b2881
+# ╠═62d7769b-dcdc-466b-84a7-df500ffe4b16
+# ╠═b4d1ea02-c7ad-4269-acc2-d34ea75acd26
 # ╠═18262d61-d4d2-457e-a79a-77c3a365042c
 # ╠═88fa84cd-b785-4bf7-841e-3c623c381c86
-# ╠═2b027eda-4ec5-4315-bc7e-5a7774abac9b
-# ╠═6794a588-7c72-47dd-8395-164545b6132a
-# ╠═3835fef4-ab2a-44bf-a3b5-0bce1a70c605
-# ╠═0050899c-2616-4f7f-aac7-c0a2eefb6ca2
-# ╠═36c193e0-7531-4120-91e0-3e0ecdcb65bc
-# ╠═0d12ebec-75c3-4f15-8098-9867bb6a3626
-# ╠═b81de22d-469a-41e4-acf4-538d395bc653
-# ╠═4f58a7ac-ae6e-4e43-9d59-8a5d1eab6554
-# ╠═9982c4a7-bd44-4f04-a241-2eeeefaaa0fb
-# ╠═a59ce994-3c51-4613-ac10-7fda447ef3af
+# ╟─5fde82bd-73bd-4528-8116-853719d74fd0
+# ╠═953a59ea-4be0-4dd9-a49e-68a54b3d6c1a
+# ╠═db91d85b-d880-44ba-b794-f957f56fc18b
+# ╠═ce4e3b7a-e3d7-47bb-9ca1-c68282c05ed8
+# ╠═0d47164f-b858-4f3a-9fa7-d89b64ceef38
+# ╠═b8442feb-8058-4fd2-bf7d-e24d81e3766f
+# ╠═6dd0fc08-8aeb-44f2-8d86-7d8e1d1127e1
+# ╠═79cfc8c3-96c1-4de9-9aab-b49e097c2019
+# ╠═4c90a7ae-8816-4cd0-96fe-5ce321e6d149
+# ╠═1d625348-8118-4575-b9b2-0a9408f4eed7
+# ╠═7a44f4fe-ea5a-45bf-b803-3caf2e1dead7
+# ╠═6c8e89db-fdde-406c-bbbb-d7f601fef58b
+# ╠═02f41616-47c7-4ec9-9200-784627b28f67
+# ╠═7161e4a5-2de4-408a-b176-d1c7d49075de
+# ╠═35456f38-1c04-4d81-9bfb-e87b3cf792a0
+# ╠═f4f9359d-411d-49d9-9813-6d27717ecbba
+# ╠═0384a698-d1e6-400b-b86a-f207eb3d09a3
+# ╟─cecc50d1-69ff-4ac0-8878-e606997d0d9d
+# ╠═f7223612-dd00-4c49-b289-97e7f6acd2d7
+# ╠═5f6c6ca9-3b53-429a-b381-732f3a45f7d4
+# ╠═570c4b96-7aa4-4f79-8217-cf7b9ed127d7
+# ╠═b36bf2e8-5e68-4cdf-bf27-7fb65467be18
+# ╠═4e10811f-9fa3-4b9d-8a88-f2b86f33fac3
