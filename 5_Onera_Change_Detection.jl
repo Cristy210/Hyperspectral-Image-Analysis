cb8c9fd0994ecb521549800ecb30dcc458952e0d
Suhas Cristy Mathey <132161396+Cristy210@users.noreply.github.com>
2024-10-24 19:45:26 -0400

    Cluster Alignment for different cities

--- 5_Onera_Change_Detection.jl
+++ 5_Onera_Change_Detection.jl
@@ -30,7 +30,7 @@ main {
 """
 
 # ╔═╡ 9abb2611-c08b-4307-8cc2-911302598f12
-@bind City Select(["beirut"])
+@bind City Select(["abudhabi", "mumbai", "aguasclaras", "beihai", "beirut", "bercy", "bordeaux", "cupertino", "hongkong", "nantes", "paris", "pisa", "rennes", "saclay_e"])
 
 # ╔═╡ 383efc0f-b775-4851-8a55-0ede19c78239
 Path = Dict("_T1" => joinpath(@__DIR__, "Onera Satellite Change Detection dataset - Images", City, "imgs_1_rect"), "_T2" => joinpath(@__DIR__, "Onera Satellite Change Detection dataset - Images", City, "imgs_2_rect"))
@@ -85,32 +85,29 @@ with_theme() do
 end
 
 # ╔═╡ 432bd598-d170-43cc-8519-d0f2758fb083
-function affinity(cube; max_nz=10, chunksize=minimum(size(cube)[1:2]),
+begin
+function affinity(X::Matrix; max_nz=10, chunksize=isqrt(size(X,2)),
 	func = c -> exp(-2*acos(clamp(c,-1,1))))
 
-	# Verify that chunksize divides the total number of pixels
-	mod(prod(size(cube)[1:2]), chunksize) == 0 ||
-		error("chunksize must divide the total number of pixels")
-
 	# Compute normalized spectra (so that inner product = cosine of angle)
-	X = permutedims(reshape(cube, :, size(cube,3)))
 	X = mapslices(normalize, X; dims=1)
 
 	# Find nonzero values (in chunks)
 	C_buf = similar(X, size(X,2), chunksize)    # pairwise cosine buffer
 	s_buf = Vector{Int}(undef, size(X,2))       # sorting buffer
 	nz_list = @withprogress mapreduce(vcat, enumerate(Iterators.partition(1:size(X,2), chunksize))) do (chunk_idx, chunk)
-		# Compute cosine angles (for chunk) and store in buffer
-		mul!(C_buf, X', view(X, :, chunk))
+		# Compute cosine angles (for chunk) and store in appropriate buffer
+		C_chunk = length(chunk) == chunksize ? C_buf : similar(X, size(X,2), length(chunk))
+		mul!(C_chunk, X', view(X, :, chunk))
 
 		# Zero out all but `max_nz` largest values
-		nzs = map(chunk, eachcol(C_buf)) do col, c
+		nzs = map(chunk, eachcol(C_chunk)) do col, c
 			idx = partialsortperm!(s_buf, c, 1:max_nz; rev=true)
 			collect(idx), fill(col, max_nz), func.(view(c,idx))
 		end
 
 		# Log progress and return
-		@logprogress chunk_idx/(size(X,2) ÷ chunksize)
+		@logprogress chunk_idx/cld(size(X,2),chunksize)
 		return nzs
 	end
 
@@ -119,6 +116,9 @@ function affinity(cube; max_nz=10, chunksize=minimum(size(cube)[1:2]),
 	cols = reduce(vcat, getindex.(nz_list, 2))
 	vals = reduce(vcat, getindex.(nz_list, 3))
 	return sparse([rows; cols],[cols; rows],[vals; vals])
+end
+affinity(cube::Array{<:Real,3}; kwargs...) =
+	affinity(permutedims(reshape(cube, :, size(cube,3))); kwargs...)
 end
 
 # ╔═╡ 6f1b577a-6042-4fcc-a21b-0b351e829dfd
@@ -135,7 +135,7 @@ A_2 = cachet(joinpath(CACHEDIR, "Affinity_$(City)_T2$max_nz.bson")) do
 end
 
 # ╔═╡ 48267064-d704-439f-bd9e-f2f620a902e3
-function embedding(A, k; seed=0)
+function embedding(A, k; seed=0, kwargs...)
 	# Set seed for reproducibility
 	Random.seed!(seed)
 
@@ -145,7 +145,7 @@ function embedding(A, k; seed=0)
 	L = Symmetric(I - (Dsqrinv * A) * Dsqrinv)
 
 	# Compute eigenvectors
-	decomp, history = partialschur(L; nev=k, which=:SR)
+	decomp, history = partialschur(L; nev=k, which=:SR, kwargs...)
 	@info history
 
 	return mapslices(normalize, decomp.Q; dims=2)
@@ -154,11 +154,8 @@ end
 # ╔═╡ 8b04f5d4-6266-41f6-bacd-a0b091e37f19
 n_clusters = 6
 
-# ╔═╡ 2466897c-2361-4ad7-b007-95a14b104bd0
-V_1 = embedding(A_1, n_clusters)
+# ╔═╡ e2baaf78-4621-4f7a-bdf8-d5975db35d82
 
-# ╔═╡ 6689dd33-a0db-4fa7-9739-4ca079e359dd
-V_2 = embedding(A_2, n_clusters)
 
 # ╔═╡ 69646bb2-f708-43dc-b58f-84b1fd8438c2
 function batchkmeans(X, k, args...; nruns=100, kwargs...)
@@ -182,31 +179,48 @@ function batchkmeans(X, k, args...; nruns=100, kwargs...)
 	return sort(runs; by=run->run.totalcost)
 end
 
+# ╔═╡ be35e249-a19b-41ea-b110-28b21a352b58
+aligned_assignments(clusterings, baseperm=1:maximum(first(clusterings).assignments)) = map(clusterings) do clustering
+	# New labels determined by simple heuristic that aims to align different clusterings
+	thresh! = a -> (a[a .< 0.2*sum(a)] .= 0; a)
+	alignments = [thresh!(a) for a in eachrow(counts(clusterings[1], clustering))]
+	new_labels = sortperm(alignments[baseperm]; rev=true)
+
+	# Return assignments with new labels
+	return [new_labels[l] for l in clustering.assignments]
+end
+
 # ╔═╡ 66ab6df2-0eb7-4ccd-8577-8412dcf391eb
-spec_clusterings_1 = batchkmeans(permutedims(V_1), n_clusters; maxiter=1000)
+spec_clusterings_1 = cachet(joinpath(CACHEDIR, "spec-max_nz_T1$(City)-$max_nz-n_clusters-$n_clusters.bson")) do
+	V_1 = embedding(A_1, n_clusters; restarts=2000)
+	batchkmeans(permutedims(V_1), n_clusters; maxiter=1000)
+end
+
+# ╔═╡ f0ddfef4-0a2f-4b54-846e-e5def0e9c5ea
+
 
 # ╔═╡ d1c8fb7a-ee78-4601-949f-4c26b668802b
-spec_clusterings_2 = batchkmeans(permutedims(V_2), n_clusters; maxiter=1000)
+spec_clusterings_2 = cachet(joinpath(CACHEDIR, "spec-max_nz_T2$(City)-$max_nz-n_clusters-$n_clusters.bson")) do
+	V_2 = embedding(A_2, n_clusters; restarts=2000)
+	batchkmeans(permutedims(V_2), n_clusters; maxiter=1000)
+end
 
-# ╔═╡ da84ec4a-9ad1-432e-b48d-e61f98862e80
-costs_1 = [spec_clusterings_1[i].totalcost for i in 1:100]
+# ╔═╡ 0304876e-4c79-4743-aed5-8b2a7bd39c51
+spec_aligned_1 = aligned_assignments(spec_clusterings_1)
 
-# ╔═╡ ea2e6763-848f-4aae-8c67-aba2e3ddafb3
-costs_2 = [spec_clusterings_2[i].totalcost for i in 1:100]
+# ╔═╡ 842b72e2-aa38-45c5-9ddd-d68e0dc23c02
+spec_aligned_2 = aligned_assignments(spec_clusterings_2)
 
-# ╔═╡ 273b5231-39e4-4225-abd5-b9e6596719ea
-clusters_1 = spec_clusterings_1[argmin(costs_1)].assignments
+# ╔═╡ 5686b3ec-fa41-4790-b0a6-f24d44d30635
+@bind spec_clustering_idx PlutoUI.Slider(1:length(spec_clusterings_1); show_value=true)
 
-# ╔═╡ d2e56b14-dfe3-44f7-8cad-b6ea6a3960a3
-clusters_2 = spec_clusterings_2[argmin(costs_2)].assignments
+# ╔═╡ b6e51128-10cb-407e-8ec1-5f994d68494b
+mat_2 = reshape(spec_aligned_2[spec_clustering_idx], size(data_1, 1), size(data_1, 2));
 
-# ╔═╡ 512f26cb-24de-4387-b6d1-3f1a4650c1e1
-mat_1 = reshape(clusters_1, size(data_1, 1), size(data_1, 2))
+# ╔═╡ 3ef0eb99-8968-4ed1-8832-08d891e95adb
+mat_1 = reshape(spec_aligned_1[spec_clustering_idx], size(data_1, 1), size(data_1, 2));
 
-# ╔═╡ a088fef2-0966-4fb0-90f1-fb006fb8827d
-mat_2 = reshape(clusters_2, size(data_1, 1), size(data_1, 2))
-
-# ╔═╡ 7ef2c784-bd0c-4cf4-9d4a-6d4d81d8410c
+# ╔═╡ 3e909260-73a7-4529-bb97-5427cba6e2ad
 with_theme() do
 	fig = Figure(; size=(900, 600))
 	ax_1 = Axis(fig[1, 1], aspect=DataAspect(), yreversed=true)
@@ -216,6 +230,9 @@ with_theme() do
 	hm_2 = heatmap!(ax_2, mat_2; colormap=Makie.Categorical(colors))
 	fig
 end
+
+# ╔═╡ 4919cb03-443b-4782-a52f-df4c06c7dccd
+
 
 # ╔═╡ Cell order:
 # ╟─60918ff2-8f51-11ef-3a74-2def7575fac8
@@ -241,15 +258,16 @@ end
 # ╠═876cc0e8-2880-4591-b212-1eac3d2c0b9a
 # ╠═48267064-d704-439f-bd9e-f2f620a902e3
 # ╠═8b04f5d4-6266-41f6-bacd-a0b091e37f19
-# ╠═2466897c-2361-4ad7-b007-95a14b104bd0
-# ╠═6689dd33-a0db-4fa7-9739-4ca079e359dd
+# ╠═e2baaf78-4621-4f7a-bdf8-d5975db35d82
 # ╠═69646bb2-f708-43dc-b58f-84b1fd8438c2
+# ╠═be35e249-a19b-41ea-b110-28b21a352b58
 # ╠═66ab6df2-0eb7-4ccd-8577-8412dcf391eb
+# ╠═f0ddfef4-0a2f-4b54-846e-e5def0e9c5ea
 # ╠═d1c8fb7a-ee78-4601-949f-4c26b668802b
-# ╠═da84ec4a-9ad1-432e-b48d-e61f98862e80
-# ╠═ea2e6763-848f-4aae-8c67-aba2e3ddafb3
-# ╠═273b5231-39e4-4225-abd5-b9e6596719ea
-# ╠═d2e56b14-dfe3-44f7-8cad-b6ea6a3960a3
-# ╠═512f26cb-24de-4387-b6d1-3f1a4650c1e1
-# ╠═a088fef2-0966-4fb0-90f1-fb006fb8827d
-# ╠═7ef2c784-bd0c-4cf4-9d4a-6d4d81d8410c
+# ╠═0304876e-4c79-4743-aed5-8b2a7bd39c51
+# ╠═842b72e2-aa38-45c5-9ddd-d68e0dc23c02
+# ╠═5686b3ec-fa41-4790-b0a6-f24d44d30635
+# ╠═b6e51128-10cb-407e-8ec1-5f994d68494b
+# ╠═3ef0eb99-8968-4ed1-8832-08d891e95adb
+# ╠═3e909260-73a7-4529-bb97-5427cba6e2ad
+# ╠═4919cb03-443b-4782-a52f-df4c06c7dccd
